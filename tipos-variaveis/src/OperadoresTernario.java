4c1d03865b5ab52cb3ce8829a4ee7349840b11cf
mazolenodutra <mazolenodutra@gmail.com>
2024-05-07 16:16:22 -0300

    Exemplo utiizando If e Else e uso do operador ternário

---
+++ tipos-variaveis/src/OperadoresTernario.java
@@ -0,0 +1,23 @@
+public class OperadoresTernario {
+    public static void main(String[] args) {
+        // classe Operadores.java
+        int a, b;
+
+        a = 6;
+        b = 6;
+
+        String resultado = a == b ? "verdadeiro" : "falso";
+        /*
+         * exmplo comum de if e else
+         * if (a == b) {
+         * resultado = "verdadeiro";
+         * } else {
+         * resultado = "falso";
+         * }
+         */
+
+        // Operador ternário
+
+        System.out.println(resultado);
+    }
+}
