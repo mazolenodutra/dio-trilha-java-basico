b208ae777b2df13894f31cf8db7422f3d3d7b776
mazolenodutra <mazolenodutra@gmail.com>
2024-05-07 16:08:38 -0300

    Exemplos de concatenação em Java

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,23 @@
+public class Operadores {
+    public static void main(String[] args) {
+        // qual o resultado das expressoes abaixo?
+        String concatenacao = "?";
+        System.out.println(concatenacao);
+
+        concatenacao = 1 + 1 + 1 + "1";
+        System.out.println(concatenacao);
+
+        concatenacao = 1 + "1" + 1 + 1;
+        System.out.println(concatenacao);
+
+        concatenacao = 1 + "1" + 1 + "1";
+        System.out.println(concatenacao);
+
+        concatenacao = "1" + 1 + 1 + 1;
+        System.out.println(concatenacao);
+
+        concatenacao = "1" + (1 + 1 + 1);
+        System.out.println(concatenacao);
+
+    }
+}
