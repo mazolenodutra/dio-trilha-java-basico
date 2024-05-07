fa81bbdd8d95efb584c0dc901fe36a2edcefc46f
mazolenodutra <mazolenodutra@gmail.com>
2024-05-07 14:26:19 -0300

    Atualização de código

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,15 @@
 public class TiposVariaveis {
+    /**
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        int numero = 5;
+        numero = 10;
+        final double VALOR_DE_PI = 3.14; // CONSTANTE
+
+        System.out.println(numero + VALOR_DE_PI);
+
     }
 }
