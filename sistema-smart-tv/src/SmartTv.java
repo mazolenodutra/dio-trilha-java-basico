475783e73c19f9585e3d63c3ca3b8ebd4e034d73
mazolenodutra <mazolenodutra@gmail.com>
2024-05-08 12:12:16 -0300

    Métodos em Java

---
+++ sistema-smart-tv/src/SmartTv.java
@@ -0,0 +1,35 @@
+public class SmartTv {
+    boolean ligada = false;
+    int canal = 1;
+    int volune = 25;
+
+    public void aumentarCanal(int novoCanal) {
+        canal = novoCanal;
+    }
+
+    public void ligar() {
+        ligada = true;
+    }
+
+    public void desligar() {
+        ligada = false;
+    }
+
+    public void aumentarVolume() {
+        System.out.println("Aumentado o volume para: " + volune);
+        volune++;
+    }
+
+    public void diminuirVolume() {
+        System.out.println("Diminuindo o volume para: " + volune);
+        volune--;
+    }
+
+    public void aumentarCanal() {
+        canal++;
+    }
+
+    public void diminuirCanal() {
+        canal--;
+    }
+}
