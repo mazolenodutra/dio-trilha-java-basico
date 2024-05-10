7fd61cefe7d1012f4b7e4db929b0e4e1afd1003b
mazolenodutra <mazolenodutra@gmail.com>
2024-05-10 15:37:23 -0300

    Adição do código para verificar o status do boletim escolar

---
+++ controle-fluxo/src/BoletimEscolar.java
@@ -0,0 +1,12 @@
+public class BoletimEscolar {
+    public static void main(String[] args) {
+        int nota = 5;
+        if (nota >= 7) {
+            System.out.println("Aprovado");
+        } else if (nota >= 5 && nota < 7) {
+            System.out.println("Recuperação");
+        } else {
+            System.out.println("Reprovado");
+        }
+    }
+}
